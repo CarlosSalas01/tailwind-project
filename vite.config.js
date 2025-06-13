0c5b4411b25311625044ad8594843efc74018295
Carlos <jcarloshsalas01@gmail.com>
2025-06-13 11:05:50 -0600

    Cambio en vite para nueva configuración

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-// vite.config.js
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   root: "./",
   build: {
     outDir: "dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
   },
 });
